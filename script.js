e248340aa4c74ffe41f9d90933b156a9f2fc5252
Duouk2000 <duouk2000@gmail.com>
2024-01-28 00:03:36 +0000

    added inital js code to check api key is working

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("test")
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+var apiKey = "20be103c6242f46290b71482f79294de"
+var city = "London"
+
+var apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+
+fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+        var tempC = data.main.temp - 273.15;
+
+      // console log temp
+      console.log("Temperature: " + tempC.toFixed(2) + "(C)");
+     // console log wind
+      console.log("Wind Speed: " + data.wind.speed + " KPH");
+      // console log humidity
+      console.log("Humidity: " + data.main.humidity + "%");
+
+    })
